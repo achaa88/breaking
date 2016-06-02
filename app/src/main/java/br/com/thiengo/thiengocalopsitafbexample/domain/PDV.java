0817d9ad0acc17aeafdfb85dc41e061e9e96ebf1
Ellen <ellen.sousa@gmail.com>
2016-06-02 18:05:56 -0300

    atributos ponto de venda e produtos

--- app/src/main/java/br/com/thiengo/thiengocalopsitafbexample/domain/PDV.java
+++ app/src/main/java/br/com/thiengo/thiengocalopsitafbexample/domain/PDV.java
@@ -4,11 +4,132 @@ package br.com.thiengo.thiengocalopsitafbexample.domain;
  * Created by Vitor on 02/06/2016.
  */
 public class PDV {
-   Gondula gondulas[];
-    String endereco;
+
+    private Gondula gondulas[];
+
+    private String CNPJ;
+    private String nome;
+    private String GPA;
+    private String bandeira;
+    private String endereco;
+    private String bairro;
+    private String CEP;
+    private String cidade;
+    private String UF;
+    private String telefone;
+
+    private String latitude;
+    private String longitude;
+
+    // Constructors
     public PDV(String endereco){
         this.endereco=endereco;
     }
+    public PDV(String CNPJ, String nome, String GPA, String bandeira, String endereco, String bairro, String cidade, String UF) {
+        this.CNPJ = CNPJ;
+        this.nome = nome;
+        this.GPA = GPA;
+        this.bandeira = bandeira;
+        this.endereco = endereco;
+        this.bairro = bairro;
+        this.cidade = cidade;
+        this.UF = UF;
+    }
+    //
+    public void showinMap(){}
+    public boolean checkin(){return true;} // eu acho que nao tem que ficar aqui
+    public boolean checkout(){return true;}  // eu acho que nao tem que ficar aqui
+
+    // Getters and Setters
+    public String getCEP() {
+        return CEP;
+    }
+
+    public void setCEP(String CEP) {
+        this.CEP = CEP;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public Gondula[] getGondulas() {
+        return gondulas;
+    }
+
+    public void setGondulas(Gondula[] gondulas) {
+        this.gondulas = gondulas;
+    }
+
+    public String getCNPJ() {
+        return CNPJ;
+    }
+
+    public void setCNPJ(String CNPJ) {
+        this.CNPJ = CNPJ;
+    }
+
+    public String getnome() {
+        return nome;
+    }
+
+    public void setnome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getGPA() {
+        return GPA;
+    }
+
+    public void setGPA(String GPA) {
+        this.GPA = GPA;
+    }
+
+    public String getbandeira() {
+        return bandeira;
+    }
+
+    public void setbandeira(String bandeira) {
+        this.bandeira = bandeira;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getUF() {
+        return UF;
+    }
+
+    public void setUF(String UF) {
+        this.UF = UF;
+    }
+
+
 }
 //fazer check in
 
--- app/src/main/java/br/com/thiengo/thiengocalopsitafbexample/domain/SKU.java
+++ app/src/main/java/br/com/thiengo/thiengocalopsitafbexample/domain/SKU.java
@@ -4,8 +4,92 @@ package br.com.thiengo.thiengocalopsitafbexample.domain;
  * Created by Vitor on 02/06/2016.
  */
 public class SKU {
-    String nome;
-    double preco;
+
+    private int Id;
+    private String nome;
+    private String EAN;
+    private int categoria;
+    private int subCategoria;
+    private String urlFoto;
+
+    // Precisa diferenciar entre atacado e varejo??
+    private Double precoMaximo;
+    private Double precoMinimo;
+    private Double precoSugerido;
+
+    // Construtores
 
 
+    // Getters and Setters
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEAN() {
+        return EAN;
+    }
+
+    public void setEAN(String EAN) {
+        this.EAN = EAN;
+    }
+
+    public int getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(int categoria) {
+        this.categoria = categoria;
+    }
+
+    public int getSubCategoria() {
+        return subCategoria;
+    }
+
+    public void setSubCategoria(int subCategoria) {
+        this.subCategoria = subCategoria;
+    }
+
+    public String getUrlFoto() {
+        return urlFoto;
+    }
+
+    public void setUrlFoto(String urlFoto) {
+        this.urlFoto = urlFoto;
+    }
+
+    public Double getPrecoMaximo() {
+        return precoMaximo;
+    }
+
+    public void setPrecoMaximo(Double precoMaximo) {
+        this.precoMaximo = precoMaximo;
+    }
+
+    public Double getPrecoMinimo() {
+        return precoMinimo;
+    }
+
+    public void setPrecoMinimo(Double precoMinimo) {
+        this.precoMinimo = precoMinimo;
+    }
+
+    public Double getPrecoSugerido() {
+        return precoSugerido;
+    }
+
+    public void setPrecoSugerido(Double precoSugerido) {
+        this.precoSugerido = precoSugerido;
+    }
 }
